611c4b762a5ef2950d2d672c3275393d23420729
Anthony Re <re@towson.edu>
2018-09-23 00:33:09 -0400

    refactored some methods with lodash/fp

--- client/Main/main.dal.js
+++ client/Main/main.dal.js
@@ -4,21 +4,19 @@ import { get } from 'axios';
 
 export const getMous = () => get( '/api/mou' );
 
-export const sumValues = mous =>
-    mous.reduce( ( acc, { value } ) => acc + value, 0 );
+import {
+    sumBy,
+    reduce,
+    countBy
+} from 'lodash/fp';
 
-export const valueByClient = mous =>
-    mous.reduce( ( acc, { client_name, value } ) => {
-        acc[ client_name ] = acc[ client_name ]
-            ? acc[ client_name ] + value
-            : value;
-        return acc;
-    }, {});
+export const sumValues = sumBy( 'value' );
 
-export const countByClientType = mous =>
-    mous.reduce( ( acc, { client_type } ) => {
-        acc[ client_type ] = acc[ client_type ]
-            ? acc[ client_type ] + 1
-            : 1;
-        return acc;
-    }, {});
+export const valueByClient = reduce( ( acc, { client_name, value } ) => {
+    acc[ client_name ] = acc[ client_name ]
+        ? acc[ client_name ] + value
+        : value;
+    return acc;
+}, {});
+
+export const countByClientType = countBy( 'client_type' );
